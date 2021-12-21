42c785c5b759abb4faa03d212d2ced53c0ed28be
Biney07 <93480288+Biney07@users.noreply.github.com>
2021-12-22 00:29:02 +0100

    verzion 3
    biney

--- components/events/ReadMe.md
+++
@@ -1,1 +0,0 @@
-"# MarakliWeb" 
--- components/events/event.js
+++ components/events/event.js
@@ -1,14 +1,11 @@
-const ev = document.querySelectorAll('.eventetup')
-ev.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+function getInner() {
+    var id_div = event.target.id;
+    var div = document.getElementById(id_div);
+    console.log(div);
+    var inner_div = div.innerHTML;
+    console.log(inner_div);
+    document.getElementById("overlay").innerHTML = '<div id="text">' + inner_div + '</div>';
+    document.getElementById("overlay").style.display = "block";
 
-})
 
-function removeActiveClasses() {
-    ev.forEach(panel => {
-        panel.classList.remove('active')
-    })
 }
