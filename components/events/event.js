9064461649c688866e9597d2da6766ced4ffab12
Biney07 <93480288+Biney07@users.noreply.github.com>
2021-12-22 21:01:28 +0100

    Past event almost finito
    bir ta ha nonen

---
+++ components/events/event.js
@@ -0,0 +1,21 @@
+const ev = document.querySelectorAll('.eventet')
+ev.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+
+})
+const button = document.querySelectorAll('.read')
+
+function removeActiveClasses() {
+    ev.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+document.getElementById('going').onClick = function() {
+    ev.forEach(panel => {
+        panel.classList.remove('active')
+    })
+
+}
