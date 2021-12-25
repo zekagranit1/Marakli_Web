01d52ec4005d078839177ca43eff2e2f7c3ffabd
zekazeka1 <zekagranit1@gmail.com>
2021-12-25 20:40:00 +0100

    fixed issue with background pic

--- components/general components/js/home1,1.js
+++ components/general components/js/home1,1.js
@@ -31,7 +31,7 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
+  var scrolled = (winScroll / height) * 150;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
 
