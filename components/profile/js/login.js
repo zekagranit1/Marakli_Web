f587208f059634b7d5f9435cea90039f3e2bc827
zekagranit1 <64788935+zekagranit1@users.noreply.github.com>
2021-12-26 21:40:37 +0100

    validimi i log init
    kur te shkrun edhe email edhe pass mir (dmth pa gabime drejtshkrimore)athere te qon butoni sign in ne home

--- components/profile/js/login.js
+++ components/profile/js/login.js
@@ -9,3 +9,25 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
 });
+
+function validation(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('pass').value;
+
+    var emailcheck = /^[A-Za-z0-9_]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z]{2,6}$/
+    var passwordcheck = /^[A-Z]+[A-Za-z.!@#$%^&*-+]+[0-9]{3}$/
+
+
+    if(!emailcheck.test(email)) {
+        alert("**JU LUTEM SHKRUAJENI MIRE EMAILIN**")
+    } 
+    if(!passwordcheck.test(password)) {
+        alert("**PASSWORD DUHET TE PERMBAJE SHKRONJEN E PARE TE MADHE DHE 3 NUMRA NE FUND**")
+    
+    } 
+    else if (emailcheck.test(email) && passwordcheck.test(password)) {
+        window.location = "../general components/home1,1.html"
+        alert('korrekt');
+    }
+
+}
