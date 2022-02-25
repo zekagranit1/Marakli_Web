48677f1bf0f3f7f9faa59ab215aec3fc0459ecbb
Biney07 <as51465@ubt-uni.net>
2022-02-25 17:43:42 +0100

    navbar

--- components/general components/index.php
+++ components/general components/index.php
@@ -5,11 +5,11 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='./css/home1,1.css' rel='stylesheet' type='text/css' />
+    <link href='./css/index.css' rel='stylesheet' type='text/css' />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-    <script src='./js/home1,1.js'></script>
+    <script src='./js/index.js'></script>
 
     <style>
 
--- components/general components/navbar.php
+++ components/general components/navbar.php
@@ -6,20 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="./js/"></script>
-    <link rel="stylesheet" href="./css/navbar.css">
+    <link rel="stylesheet" href="./css/nav_bar.css">
 </head>
 <body>
     <div id="navbar">
         <div id="nav-holder">
             <a href="#default" id="logo">Marakli T'Biciklave</a>
             <div id="navbar-right">
+            <div class="dropdown">
+                <button id = "activate" class="dropbtn">Dropdown</button>
+                 <div class="dropdown-content">
                 <a class="link" href="./index.php">Home</a>
                 <a class="link" href="../events/Eventet.php">Eventet</a>
                 <a class="link" href="../shop/shop.php">Shop</a>
-                <!-- <div>||</div> -->
                 <a class="link" href="../profile/Login.php">Log in</a>
                 <a class="active" href="../profile/Sign up.php">Join Us</a>
-            </div>
+             </div>
+        </div>
+</div>
         </div>
     </div>
     
