fe2832b2daf116e4850e41cf48566d076b4b57fa
Biney07 <93480288+Biney07@users.noreply.github.com>
2022-02-25 23:59:58 +0100

    Kerrsh - Sign in and up

--- components/profile/Sign up.php
+++ components/profile/Sign up.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 
@@ -17,38 +18,34 @@
                 <div class="form-group"></div>
                 <div class="form-wrapper">
                     <label for="">Emri:</label>
-                    <input id="emri" type="text" class="form-control" placeholder="filan">
+                    <input name="emri" type="text" class="form-control" placeholder="filan">
                 </div>
                 <div class="form-wrapper">
                     <label for="">Mbiemri:</label>
-                    <input id="mbiemri" type="text" class="form-control" placeholder="fisteku">
-                </div>
-                <div class="form-wrapper">
-                    <label for="">Creditcard Number:</label>
-                    <input id="card" type="text" class="form-control" placeholder="512*******">
+                    <input name="mbiemri" type="text" class="form-control" placeholder="fisteku">
                 </div>
                 <div class="form-wrapper">
                     <label for="">Adresa:</label>
-                    <input id="card" type="text" class="form-control" placeholder="Prishtine, Lagjja: Dardania, Rruga:Sadik Zeneli">
+                    <input name="adresa" type="text" class="form-control" placeholder="Prishtine, Lagjja: Dardania, Rruga:Sadik Zeneli">
                 </div>
                 <div class="form-wrapper">
                     <label for="">Email:</label>
-                    <input id="email" type="text" class="form-control" placeholder="email@adress.com">
+                    <input name ="email" type="text" class="form-control" placeholder="email@adress.com">
                 </div>
                 <div class="form-group">
                     <div class="form-wrapper">
                         <label for="">Password:</label>
-                        <input id="password" type="password" class="form-control" placeholder="password">
+                        <input name="password" type="password" class="form-control" placeholder="password">
                     </div>
                     <div class="form-wrapper">
                         <label for="">Konfirmo Password:</label>
-                        <input id="konfirm" type="password" class="form-control" placeholder="confirm">
+                        <input name="konfirm" type="password" class="form-control" placeholder="confirm">
                     </div>
                 </div>
                 <div class="form-group form-width">
                     <div class="form-wrapper">
                         <label for="">Mosha:</label>
-                        <input id="mosha" type="number" class="mosha form-control" placeholder="">
+                        <input name="mosha" type="number" class="mosha form-control" placeholder="">
                     </div>
                     <div class="form-wrapper gjinia">
                         <label class="gjinia-margin" for="">Gjinia:</label>
@@ -60,7 +57,8 @@
                         </div>
                     </div>
                 </div>
-                <button id="register" onclick="validation()">Register Now</button>
+                <!-- <button id="register" onclick="validation()">Register Now</button> -->
+                <button type="submit" name="register">Register Now</button>
             </form>
         </div>
     </div>
